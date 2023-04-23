2c5e843930d214d10b5f661954a15d9e3a2d3bce
dikshit-n <62920150+dikshit-n@users.noreply.github.com>
2023-04-23 19:39:52 +0530

    apr-23/23

--- basic/docs/http.js
+++ basic/docs/http.js
@@ -1,18 +1,18 @@
 const http = require('http');
-// // create a server
-// // a server is an event emitter
-// const server = http.createServer();
+// create a server
+// a server is an event emitter
+const server = http.createServer();
 
-// // listen to the event emitted by the server
-// // this method is very low level, we should actually respond to (req, res) objects.
-// server.on('connection', (socket) => {
-//     console.log('New connection...');
-// });
+// listen to the event emitted by the server
+// this method is very low level, we should actually respond to (req, res) objects.
+server.on('connection', (socket) => {
+    console.log('New connection...');
+});
 
-// // listen to a port
-// // whenever a request is sent to port 5000, this server will emit an event
-// server.listen(5000);
-// console.log('Listening to port 5000...')
+// listen to a port
+// whenever a request is sent to port 5000, this server will emit an event
+server.listen(5000);
+console.log('Listening to port 5000...')
 
 // in real world projects, we use this method
 const server = http.createServer((req, res) => {
