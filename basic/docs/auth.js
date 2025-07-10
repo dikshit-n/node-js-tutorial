d0b85dce8bc690ac410c8a72fa44f74d337b9fcb
dikshit-n <62920150+dikshit-n@users.noreply.github.com>
2025-07-10 10:43:02 +0530

    docs

---
+++ basic/docs/auth.js
@@ -0,0 +1,9 @@
+// Authentication
+    // To verify whetehr the user is who they claim they are
+// Authorization
+    // To verify whether the user has permissions for certain action
+
+// {
+    // email: String,
+    // unique: true
+// }
